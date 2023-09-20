249acdaf5aa1503f2de8976c1f37bde3317ed85b
Jannel Bielza <jbielza@hawaii.edu>
2023-09-19 17:13:56 -1000

    Main

--- Lab5/Lab5.js
+++ Lab5/Lab5.js
@@ -1,14 +1,16 @@
 // declare variables per lab 5.1 instructions
 let age = 21;
-let fav_number = 3;
+let fav_num = 3;
 let day_of_birth = 7;
 let month_of_birth = 3;
 
 // define calculations
-let calc1 = (age + fav_number / day_of_birth * month_of_birth);
+let calculations1 = age + fav_num / day_of_birth * month_of_birth;
 
-let calc2 = (age + fav_number) / day_of_birth * month_of_birth;
+let calculations2 = (age + fav_num) / day_of_birth * month_of_birth;
 
 // output calc 1 & 2 to the DOM
-document.getElementById('result1').innerHTML = calc1;
-document.getElementById('result2').innerHTML = calc2;
+document.getElementById("result1").innerHTML = calculations1;
+
+document.getElementById("result2").innerHTML = calculations2;
+
