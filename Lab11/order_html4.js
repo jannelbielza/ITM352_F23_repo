a07d742d270b095b4a94e83fd7d10251c02bfe58
Jannel Bielza <jbielza@hawaii.edu>
2023-10-26 20:32:07 -1000

    Main

---
+++ Lab11/order_html4.js
@@ -0,0 +1,42 @@
+
+function formSubmission(){
+    let quantity = Number(document.querySelector(`input[name = "qty_textbox"]`).value);  
+
+    let validationMessage = validateQuantity(quantity);
+
+    if (validationMessage!=="") {
+        document.getElementById("invalidQuantity").innerHTML = validationMessage;
+    } else {
+        //redirect to the display_purchase.html page
+        //window.location.href = `display_purchase.html?qty_textbox=${quantity}`;
+        let message = `Thank you for ordering ${quantity} things!`;
+        document.body.innerHTML = message;
+    }
+    return false; // prevents form submission
+
+}
+
+
+function validateQuantity(quantity) {
+    let errorMessage = "";
+
+    switch (true) {
+        case isNaN(quantity):
+            errorMessage = "Not a number. Please enter a non-negative quantity to order.";
+            break;
+        case quantity <= 0 && !Number.isInteger(quantity):
+            errorMessage = "Negative inventory and not an Integer. Please enter a non-negative quantity to order.";
+            break;
+        case quantity <= 0:
+            errorMessage = "Negative inventory. Please enter a non-negative quantity to order.";
+            break;
+        case !Number.isInteger(quantity):
+            errorMessage = "Not an Integer. Please enter a non-negative quantity to order.";
+            break;
+        default:
+            errorMessage = ""; // No errors
+            break;
+    }
+
+    return errorMessage;
+}
